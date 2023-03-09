e6e9ed2f3b7747d5a4c09cac6c07c751a5f01197
LiFZ <lifzcn@qq.com>
2023-03-09 11:21:06 +0800

    Update some files

--- Project/Core/Src/buzzer.c
+++
@@ -1,9 +0,0 @@
-#include "buzzer.h"
-
-void warning(uint8_t parameter_1, uint8_t parameter_2, uint8_t parameter_3)
-{
-	if(parameter_3<=parameter_1 || parameter_3>=parameter_2)
-	{
-		HAL_GPIO_WritePin(Buzzer_GPIO_Port, Buzzer_Pin, GPIO_PIN_SET);
-	}
-}
